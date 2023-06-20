529d03eb9771f7b3c9f7a4dfb9797e6a6faea178
Josh Holt <jholt@Stephanie-2.local>
2023-06-20 16:23:47 +0100

    Added autocomplete function with test values to a single input field, allowing for click and keystroke operatio. Adjusted CSS to make it clearer.

--- js/index.js
+++ js/index.js
@@ -25,12 +25,137 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    //
     
-    // Set default date to today
-    defaultDate();
+    // Setup controller object
+    controller = new FlightSearch();
+
+    // Set date to today function
+    document.getElementById('datePicker').valueAsDate = new Date();
+
+    //set up autcomplete for text input field
+    controller.autocomplete(document.getElementById('departure'), autocomplete_test);
 }
 
-// Set date to today function
-function defaultDate() {
-    document.getElementById('datePicker').valueAsDate = new Date();
+// JavaScript 'class' with all functions within
+function FlightSearch() {
+    console.log("Creating controller/model");
+
+    //Private Variables and Functions
+
+    //Set up API URL
+    var BASE_GET_URL = "https://api.tequila.kiwi.com";
+
+    var API_KEY = "j9z5EBBq-xysj_2iuZzB21Oau3kNPRl_";
+
+    // Gets locations and sets them into local storage for autocomplete
+    function getLocations() {
+
+        function onSuccess(obj) {
+            console.log("Locations retrieved", obj);
+
+            var location_data;
+        }
+    }
+
+    // Autocomplete text inputs for locations, from W3Schools @ https://www.w3schools.com/howto/howto_js_autocomplete.asp
+    this.autocomplete = function(inp, arr) {
+        /* Takes two arguments, text input and array of possible values */
+        var currentFocus;
+        // Execute function when someone writes in the text field
+        inp.addEventListener("input", function(e) {
+            var a,b, i, val = this.value;
+            // Close any open lists of autocompleted values
+            closeAllLists();
+            if (!val) {return false;}
+            currentFocus = -1;
+            // Create a DIV element that will contain the items (values)
+            a = document.createElement("DIV")
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            // Append the DIV element as a child of the autocomplete container
+            this.parentNode.appendChild(a);
+            // For each item in the array...
+            for (i=0; i < arr.length; i++) {
+                // Check if item starts with the same letter as the text field value
+                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                    // Create a DIV element for each matching element
+                    b = document.createElement("DIV");
+                    // Make the matching letters bold
+                    b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                    b.innerHTML += arr[i].substr(val.length);
+                    // Insert an input field that will hold the current array item's value
+                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                    // Execute a function when someone clicks on the item value (DIV element)
+                        b.addEventListener("click", function(e) {
+                            // Insert the value for the autocomplete text field
+                            inp.value = this.getElementsByTagName("input")[0].value;
+                            // Close list of autocompleted values, or any other open lists
+                            closeAllLists();
+                    });
+                    a. appendChild(b);
+                }
+            }
+        });
+        // Execute a function press a key on the keyboard
+        inp.addEventListener("keydown", function(e) {
+
+            var x = document.getElementById(this.id + "autocomplete-list");
+            if (x) x = x.getElementsByTagName("div");
+            if (e.keyCode == 40) {
+                // If arrow DOWN key pressed, increase currentFocus variable
+                currentFocus++;
+                // And make the current item more visible
+                addActive(x);
+            } else if (e.keyCode == 38) {
+                // If arrow UP key is pressed, decrease the currentFocus variable
+                currentFocus--;
+                // And make the current item more visible
+                addActive(x);
+            } else if (e.keyCode == 13) {
+                // If ENTER pressed, prevent form from being submitted
+                e.preventDefault();
+                if (currentFocus > -1) {
+                    // And simulate a click on the "active" item
+                    if (x) x[currentFocus].click();
+                }
+            }
+        });
+        function addActive(x) {
+            // A function to classify an item as "active"
+            if (!x) return false;
+            // Start by removing the "active" class on all items
+            removeActive(x);
+            if (currentFocus >= x.length) currentFocus = 0;
+            if (currentFocus < 0) currentFocus = (x.length - 1);
+            // Add class "autocomplete-active"
+            x[currentFocus].classList.add("autocomplete-active");
+        }
+        function removeActive(x) {
+            // A function to remove the "active" class from all autocomplete items
+;            for (var i = 0; i < x.length; i++) {
+                x[i].classList.remove("autocomplete-active");
+            }
+        }
+        function closeAllLists(elmnt) {
+            // Close all autocomplete lists in the document, except one passed as arg
+            var x = document.getElementsByClassName("autocomplete-items");
+            for (var i = 0; i < x.length; i++) {
+                if (elmnt != x[i] && elmnt != inp) {
+                    x[i].parentNode.removeChild(x[i]);
+                }
+            }
+        }
+        // Execute a function when someone clicks in the document
+        document.addEventListener("click", function(e) {
+            closeAllLists(e.target);
+        })
+    }
+
+    this.test = function() {
+        return "This does work";
+    }
 }
+
+var autocomplete_test = ['Apple', 'Banana', 'Cherry', 'Coke', 'Deez Nuts'];
