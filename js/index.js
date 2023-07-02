e0340f83ab85cede6b230c58d38dc9657456a779
Josh Holt <jholt@Stephanie-2.local>
2023-07-02 13:29:15 +0100

    Finished first iteration. Cannot remember what was added since last commit.

--- js/index.js
+++ js/index.js
@@ -167,12 +167,42 @@ function FlightSearch() {
         return parts.join("-");
     }
 
-    // Date Format Converter - to match format accepted by the API
+    // Date Format Converter - to match format accepted by the API (dd-mm-yyyy)
     function convertDateFormat(date_string) {
         const dateArray = date_string.split("-");
         const reversedArray = dateArray.reverse();
         const newDateString = reversedArray.join("/");
         return newDateString;
+    }
+
+    // Convert date to be a nicer format for display
+    function convertNiceDate(timestamp) {
+        var date = new Date(timestamp);
+        var nice = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() +
+        ' ';
+        var hours = date.getHours().toString();
+        var minutes = date.getMinutes().toString();
+        if (hours.length < 2) {
+            hours = '0' + hours;
+        }
+        if (minutes.length < 2) {
+            minutes = minutes + '0';
+        }
+        return nice + hours + ':' + minutes
+    }
+
+    function convertDuration (seconds) {
+        // Takes the duration from the flight info and converts to hours and mins
+        var minutes = seconds / 60;
+        var hours = (Math.floor(minutes / 60)).toString();
+        var remainder = (minutes % 60).toString();
+
+        // Pad remainder if it is below 10
+        if (remainder.toString() < 2) {
+            remainder = '0' + remainder;
+        }
+
+        return hours + 'h ' + remainder + 'm';
     }
 
     function startLoader() {
@@ -186,6 +216,17 @@ function FlightSearch() {
         var img = document.getElementsByClassName('loader')[0];
         img.style.display = "none";;
     }
+
+    // Update display of price limit
+    function updatePriceText() {
+        var val = document.getElementById("limit_price").value;
+
+        if (val == 2000) {
+            document.getElementById('price_input').innerHTML = 'No Limit';
+        } else {
+            document.getElementById('price_input').innerHTML = "€" + val;
+        }
+    }
     
 
     // Initiate a search call to the API with included inputs
@@ -195,11 +236,24 @@ function FlightSearch() {
         var arrive = document.getElementById("arrive").value;
         var date = document.getElementById("datePicker").value;
         var limit = document.getElementById("limit_num").value;
+        // Price limit becomes 'effectively infinite' if it at max value
+        var price_limit = document.getElementById("limit_price").value;
+        if (price_limit == 2000) {
+            price_limit = 50000;
+        }
+
+        // Exit function if location values are not in the list of locations
+        if (!(autocomplete_list.includes(depart) && autocomplete_list.includes(arrive))){
+            alert("Please enter correct departure and arrival locations");
+            return
+        }
+
+
         // Get the next day's date & convert format
         var tomorrow = convertDateFormat(incrementDate(date));
         date = convertDateFormat(date);
 
-        // Create table function to insert into the html with the appropriate results, from StackDiary.com
+        // Create table function to insert into the html with the appropriate results, from StackDiary.com @ https://stackdiary.com/tutorials/create-table-javascript/
         function createTableFromObjects(data) {
             // clear the container of any current content or table
             const container = document.getElementById('flights-container');
@@ -245,21 +299,22 @@ function FlightSearch() {
                     stopLoader();
                 }
             } else {
-                // create an entry for each flight up to a bit lower than 
-                // the limit specified
-                for (let i = 0; i < Math.round(limit/1.5); i++) {
-                    // Create a flight entry if it exists
+                // Loop through the results up until the limit
+                for (let i = 0; i < limit; i++) {
+                    // Create a flight entry if it exists             // Josh Holt H740671X
                     if (obj.data[i]) {
                         var flight = {
                             route: obj.data[i].cityFrom + " (" + obj.data[i].flyFrom +
                              ") to " + obj.data[i].cityTo + " (" + obj.data[i].flyTo + ")",
-                            depart: obj.data[i].local_departure,
-                            arrive: obj.data[i].local_arrival,
-                            duration: obj.data[i].duration.departure,
-                            cost: obj.data[i].price
+                            depart: convertNiceDate(obj.data[i].local_departure),
+                            arrive: convertNiceDate(obj.data[i].local_arrival),
+                            duration: convertDuration(obj.data[i].duration.departure),
+                            cost: '€' + obj.data[i].price
                         }
-                        // Add the flight to the flight list
-                        flights.push(flight);
+                        // Add the flight to the flight list if it is within price limit
+                        if (obj.data[i].price <= price_limit){
+                            flights.push(flight);
+                        }
                     }
                 }
 
@@ -295,19 +350,21 @@ function FlightSearch() {
             var everywhere_flights = [];
             // Success function for each AJAX call
             function onEverywhereSuccess(obj) {
-                for (let i = 0; i < limit; i++) {
+                for (let i = 0; i < Math.round(limit/1.5); i++) {
                     // Create a flight entry if it exists
                     if (obj.data[i]) {
                         var flight = {
                             route: obj.data[i].cityFrom + " (" + obj.data[i].flyFrom +
                              ") to " + obj.data[i].cityTo + " (" + obj.data[i].flyTo + ")",
-                            depart: obj.data[i].local_departure,
-                            arrive: obj.data[i].local_arrival,
-                            duration: obj.data[i].duration.departure,
-                            cost: obj.data[i].price
+                            depart: convertNiceDate(obj.data[i].local_departure),
+                            arrive: convertNiceDate(obj.data[i].local_arrival),
+                            duration: convertDuration(obj.data[i].duration.departure),
+                            cost: '€' + obj.data[i].price
                         }
-                        // Add the flight to the flight list
-                        everywhere_flights.push(flight);
+                        // Add the flight to the flight list if it is within price limit
+                        if (obj.data[i].price <= price_limit){
+                            everywhere_flights.push(flight);
+                        }
                     }
                 }
             }
@@ -327,7 +384,7 @@ function FlightSearch() {
                 data: {}, headers: {apikey: API_KEY}, success: onEverywhereSuccess});
 
                 // Push ajax request to the array of promises
-                promises.push(request);
+                promises.push(request);                                     // Josh Holt H740671X
              };
 
             $.when.apply(null, promises).done(function (){
@@ -336,7 +393,9 @@ function FlightSearch() {
                 } else {
                     // Sort the flight list by price
                     everywhere_flights.sort(function(a, b) {
-                        return parseFloat(a.cost) - parseFloat(b.cost);
+                        a = parseFloat(a.cost.slice(1));
+                        b = parseFloat(b.cost.slice(1));
+                        return a - b;
                     })
                     
                     //Limit the size of the list of flights to the limit specified
@@ -377,7 +436,7 @@ function FlightSearch() {
                 location_dictionary[auto_entry] = {code: obj.locations[i].code,
                 type: obj.locations[i].type}
             }
-        }
+        }                                                                                           // Josh Holt H740671X
 
         //Build the airport locations URL string
         var searchAirUrl = BASE_GET_URL + '/locations/dump?locale=en-US&location_types=airport&limit=15000&sort=name&active_only=true';
@@ -406,6 +465,26 @@ function FlightSearch() {
         var searchCityUrl = BASE_GET_URL + '/locations/dump?locale=en-US&location_types=city&limit=15000&sort=name&active_only=true';
         
         $.ajax(searchCityUrl, {type: "GET", data: {}, headers: {apikey: API_KEY}, success: onCitySuccess});
+
+
+        // REGION LOCATION DATA
+        function onCountrySuccess(obj) {
+            // Go through list of codes
+            for (let i = 0; i < obj.locations.length; i++) {
+                var auto_entry = convertString(obj.locations[i].name) + " (Country)";
+                // Add each entry to autocomplete list
+                autocomplete_list.push(auto_entry);
+                // Add each entry to lookup dictionary                    
+                location_dictionary[auto_entry] = {code: obj.locations[i].id,
+                    type: obj.locations[i].type}
+            }
+        }
+        
+        //Build the region locations URL string
+        var searchCountryUrl = BASE_GET_URL + '/locations/dump?locale=en-US&location_types=country&limit=15000&sort=name&active_only=true';
+                
+        $.ajax(searchCountryUrl, {type: "GET", data: {}, headers: {apikey: API_KEY}, success: onCountrySuccess});
+
 
 
         // REGION LOCATION DATA
@@ -445,7 +524,7 @@ function FlightSearch() {
         
         $.ajax(searchContinentUrl, {type: "GET", data: {}, headers: {apikey: API_KEY}, success: onContinentSuccess});
 
-        // Add 'Everywhere' as a choice for departure and arrival.
+        // FR2 Add 'Everywhere' as a choice for departure and arrival.
         var everywhere_entry = 'Everywhere';
         autocomplete_list.push(everywhere_entry);
         location_dictionary[everywhere_entry] = {code: 'anywhere',
@@ -454,8 +533,10 @@ function FlightSearch() {
 
     }
 
-    // Set listener to search button to activate search
+    // FR4 Set listener to search button to activate search
     document.getElementById("search_button").addEventListener("click", doSearch); 
+    // FR5 Set listener to price limit bar to show value
+    document.getElementById("limit_price").addEventListener("change", updatePriceText);
 }
 var autocomplete_list = [];
 var location_dictionary = {};
